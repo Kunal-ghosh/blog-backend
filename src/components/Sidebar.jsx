81e35ea5959090d78a4a00723e590a2eaf9155be
Kunal ghosh <77928225+Kunal-ghosh@users.noreply.github.com>
2025-04-28 12:05:45 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,14 +18,34 @@ const Sidebar = ({ posts, onSelectPost, activePostId }) => {
         </div>
       ))}
       <div className="sidebar-social-icons">
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <img src="https://cdn-icons-png.flaticon.com/512/733/733635.png" alt="Twitter" className="social-icon" />
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/733/733635.png"
+            alt="Twitter"
+            className="social-icon"
+          />
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <img src="https://cdn-icons-png.flaticon.com/512/145/145802.png" alt="Facebook" className="social-icon" />
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/145/145802.png"
+            alt="Facebook"
+            className="social-icon"
+          />
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" className="social-icon" />
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png"
+            alt="Instagram"
+            className="social-icon"
+          />
         </a>
       </div>
     </div>
@@ -33,29 +53,3 @@ const Sidebar = ({ posts, onSelectPost, activePostId }) => {
 };
 
 export default Sidebar;
-
-
-
-
-
-
-// import React from 'react';
-// import '../styles/App.css';
-
-// const Sidebar = ({ posts, onSelectPost, activePostId }) => {
-//   return (
-//     <div className="sidebar">
-//       {posts.map((post) => (
-//         <div
-//           key={post.id}
-//           className={`sidebar-item ${activePostId === post.id ? 'active' : ''}`}
-//           onClick={() => onSelectPost(post.id)}
-//         >
-//           {post.title}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
